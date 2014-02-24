48a2292ce5ea42cb93ef33773a7bccb1dfefab2f
devid <devid@devid-snippetbucket.(none)>
2014-02-24 16:36:53 +0530

    Change sequence down to Apps Menu in OpenERP by Tejas Tank.

---
+++ sb_fix_appscenter/__openerp__.py
@@ -0,0 +1,46 @@
+#-*- coding:utf-8 -*-
+##############################################################################
+#
+#    SnippetBucket, MidSized Business Application Solution
+#    Copyright (C) 2013-2014 http://snippetbucket.com/. All Rights Reserved.
+#    Email: snippetbucket@gmail.com, Skype: live.snippetbucket
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name' : 'sb_fix_appscenter',
+    'version' : '1.0',
+    'depends' : [],
+    'author' : 'SnipeptBucket.com',
+    'category': 'sale',
+    'description': """
+    Snippetbucket improve OpenERP Menu order,
+    Settings ->
+            All Modules
+            Updates
+            Update Module List
+            Apps Center 
+            Apply Scheduled Upgrades
+    """,
+    'website': 'http://www.snippetbucket.com',
+    'data': ['update_data.xml'],
+    'demo': [],
+    'installable': True,
+    'auto_install': True,
+    'images': [],
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
